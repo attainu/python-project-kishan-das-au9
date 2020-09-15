f9629f1034371c8ea14d29eeb922cf56f2687132
kishan-das-au9 <kishandas909@gmail.com>
2020-09-15 21:58:40 +0530

    booking class. THis will book a cab

---
+++ rider/booking.py
@@ -0,0 +1,14 @@
+from dbconnect import DBHelper
+
+
+class Booking:
+    def findDriver(self,sourcelat,sourcelon, destlat, destlon):
+        print(sourcelat,sourcelon,destlat,destlon)
+        cabList= DBHelper().getCab(sourcelat,sourcelon)
+        if len(cabList) == 1:
+            confirmation = int(input("We have found 1 cab. Press 1 to confirm booking, press 2 to exit : "))
+            if confirmation == 1:
+                print("Cab Driver {} is on his way to pick you up. Cab number is {}".format(cabList[0][1],cabList[0][2]))
+                return {"driver_id": cabList[0][0]}
+        else:
+            print("No cabs are available")
